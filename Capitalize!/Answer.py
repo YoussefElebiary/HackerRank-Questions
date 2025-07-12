76c6d5f46eaa0eb384497d0251ea69fea6a181d7
unknown <youssefelebiary2005@gmail.com>
2025-07-12 17:42:11 +0300

    Capitalize!

---
+++ Capitalize!/Answer.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    ans = ""
+    for i in range(len(s)):
+        if i == 0 and s[i].isalpha():
+            ans += s[i].upper()
+            continue
+        if s[i - 1] == ' ' and s[i].isalpha():
+            ans += s[i].upper()
+            continue
+        ans += s[i]
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
